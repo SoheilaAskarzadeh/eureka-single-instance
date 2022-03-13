5516b0f918e9d0824676345977c2547e301cc96f
askarzadeh <s.askarzadeh@behpardaz.net>
2022-03-13 19:09:29 +0330

    init eureka single instance app

---
+++ registry-usage/src/test/java/com/registryusage/RegistryUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registryusage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
